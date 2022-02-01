9a7aebc45e5f5049467792f4c6f74aaefcee4cb8
KotadiyaYash14 <88480612+KotadiyaYash14@users.noreply.github.com>
2022-02-01 17:15:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ===== typing animation ===== */
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","UI/UX Designer","Student"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+
